0514b26f99651b430b6bbce11e4fa322aa3ae5cc
xinliu <xinliu1230@gmail.com>
2016-01-19 22:56:53 -0800

    datastore/memcache step 4

--- base_project/base/src/main/java/cs263w16/DatastoreServlet.java
+++ base_project/base/src/main/java/cs263w16/DatastoreServlet.java
@@ -11,39 +11,57 @@ import com.google.appengine.api.memcache.*;
 @SuppressWarnings("serial")
 public class DatastoreServlet extends HttpServlet {
   DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
       resp.setContentType("text/html");
       resp.getWriter().println("<html><body>");
 
-      String keyname = req.getParameter("key");
+      String keyname = req.getParameter("keyname");
       String value = req.getParameter("value");
 
       if(keyname == null && value == null) {
 	//Display every element of kind TaskData in the datastore
-	  resp.getWriter().println("All elements of kind TaskData:");
+	  resp.getWriter().println("All elements of kind TaskData in Datastore:");
 	  Query q = new Query("TaskData");
 	  PreparedQuery pq = datastore.prepare(q);
+	  List<String> keys = new LinkedList<String>();
 	  for (Entity ent : pq.asIterable()) {
-	    resp.getWriter().println( "<br />key: " + ent.getKey().getName() + " value: " + ent.getProperty("value") );
+	    resp.getWriter().println( "<br />" + ent.getKey().getName() + ": " + ent.getProperty("value") );
+		keys.add( ent.getKey().getName() ); 
+	  }
+	  Map<String, Object> map = syncCache.getAll(keys);
+	  Set<String> memKeys = map.keySet();
+	  resp.getWriter().println("<br />All elements of kind TaskData in Memcache:");
+	  for(String key : memKeys) {
+		resp.getWriter().println( "<br />" + key + ": " + (String)map.get(key) );
 	  }
 	
       } else if(keyname != null && value == null) {
+	//check memcache for the key
+	String memValue = (String) syncCache.get(keyname);
 	Key entKey = KeyFactory.createKey("TaskData", keyname);
 	try{
 	  Entity ent = datastore.get(entKey);
-	  resp.getWriter().println(  "key: " + keyname + " value: " + ent.getProperty("value")  );
+	  if(memValue != null) {
+		resp.getWriter().println(keyname + ": " + memValue +" (Both)");
+	  } else {
+		resp.getWriter().println(keyname + ": " + ent.getProperty("value") + " (DataStore)");
+		syncCache.put(keyname, ent.getProperty("value") );
+	  }
+	  //resp.getWriter().println(  "key: " + keyname + " value: " + ent.getProperty("value")  );
 	} catch(EntityNotFoundException e) {
-	  resp.getWriter().println( "cannot find " + keyname );
+	  resp.getWriter().println( "Neither" );
 	}
       } else if(keyname != null && value != null) {
 	Entity tne = new Entity("TaskData", keyname);
 	tne.setProperty("value", value);
 	Date date = new Date();
-	tne.setProperty( "createDate", date );
+	tne.setProperty( "date", date );
 	datastore.put(tne);
-	resp.getWriter().println( "Stored " + keyname + " and " + value + " in Datastore" );
+	syncCache.put(keyname, value);
+	resp.getWriter().println( "Stored " + keyname + " and " + value + " in Datastore and Memcache" );
       } else {
         resp.getWriter().println("ERROR: incorrect parameters have been passed in.");
       }
